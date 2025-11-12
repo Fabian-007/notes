230fbf9eb6e2b936168236fd3c471c34aa3cb5fb
Fabian Nobert <fabiannobert.007@gmail.com>
2025-11-12 04:56:08 +0100

    uibuild

--- index.js
+++ index.js
@@ -6,64 +6,88 @@ app.use(cors());
 //serve the frontend build
 app.use(express.static('dist'));
 app.use(express.json());
+require('dotenv').config();
+const Note = require('./models/note')
 
 
+const requestLogger = (request, response, next) => {
+  console.log('Method:', request.method);
+  console.log('Path:  ', request.path); 
+  console.log('Body:  ', request.body);
+  console.log('Params:  ', request.params);
+  console.log('---');
+  next();
+} 
+app.use(requestLogger)
 
-let notes = [
-  {
-    id: "1",
-    content: "HTML is easy",
-    important: true
-  },
-  {
-    id: "2",
-    content: "Browser can execute only JavaScript",
-    important: false
-  },
-  {
-    id: "3",
-    content: "GET and POST are the most important methods of HTTP protocol",
-    important: true
-  }
-]
+const password = process.argv[2];
 
-// const app = http.createServer((request, response) =>{
-//     response.writeHead(200, {'Content-Type': 'application/json'});
-//     response.end(JSON.stringify(notes));
-// })
+
 
 app.get('/api/notes', (request, response) => {
-    response.json(notes);
+
+  Note.find({}).then(notes => {
+    response.json(notes)
+  })        
 })
-app.get('/api/notes/:id', (request, response) => {
+
+//fetch a single note by id
+app.get('/api/notes/:id', (request, response, next) => {
 console.log(request.params);
 const id = request.params.id
-const note = notes.find(note => note.id === id)
-if(note){
+Note.findById(id).then(note => { 
+  if(note){
     response.json(note)
 }
 else{
     //overwrite default 200 status code with 404 not found
-    response.status(404).json({error: 'Note not found'}).end()
+    response.status(404).end()
 }
-} )
 
-app.delete('/api/notes/:id', (request, response) => {
-    const id = request.params.id
-    notes = notes.filter(note => note.id !== id)
-    response.status(204).end()
+})
+.catch(error => next(error))
 })
 
-//generate new id number for new note 
-const generateId = () => {
-const maxId = notes.length > 0
-? Math.floor(Math.random() * 1000000)
-: 0 
-return String(maxId + 1)
-}
+//updating a note using its id
+app.put('/api/notes/:id', (request, response, next) => {
+    const id = request.params.id
+    const body = request.body
+
+    const {content, important} = body 
+    Note.findById(id)
+    .then(note => {
+      if (!note) {
+        return response.status(404).end()
+      }
+
+      note.content = content 
+      note.important = important
+      return note.save().then(updatedNote => {
+        response.json(updatedNote)
+      })
+    })
+    .catch(error => next(error))
+})
 
 
-app.post('/api/notes', (request, response) => {
+//deleting a note using its id
+app.delete('/api/notes/:id', (request, response) => {
+    const id = request.params.id
+    Note.findByIdAndDelete(id).then(result => {
+    response.status(204).end()
+}).catch(error => next(error))
+})
+
+// //generate new id number for new note 
+// const generateId = () => {
+// const maxId = notes.length > 0
+// ? Math.floor(Math.random() * 1000000)
+// : 0 
+// return String(maxId + 1)
+// }
+
+
+app.post('/api/notes', (request, response, next) => {
 
     const body = request.body
     console.log(body);
@@ -73,18 +97,42 @@ app.post('/api/notes', (request, response) => {
             error: 'content missing'
         })
     }
-
-    const note  = {
+    
+    //create a new note object with mongoose model
+    const note  = new Note({
         content: body.content,
         important: body.important || false,
-        id: generateId()
-    }
-    notes = notes.concat(note)
-    response.json(note)
+        // id: generateId(),
+    })
+
+    note.save().then(savedNote => {
+        response.json(savedNote)
+    })
+    .catch(error => next(error))
   })
+
+
+  const unknownEndpoint = (request, response) => {
+    response.status(404).send({ error: 'unknown endpoint' })
+}
+   app.use(unknownEndpoint)  
+
+  const errorHandler = (error, request, response, next) => {
+    console.error(error.message)
+    // handle invalid id error
+    if (error.name === 'CastError') {
+        return response.status(400).send({ error: 'malformatted id' })
+    }
+    // handle validation error
+    else if (error.name === 'validationError'){
+      return response.status(400).json({error:error.message})
+    }
+    next(error)
+}
+
+app.use(errorHandler) 
   
 
-
-const PORT = process.env.PORT || 3001
+const PORT = process.env.PORT
 app.listen(PORT)
 console.log(`Server running on port ${PORT}`);
---
+++ mongo.js
@@ -0,0 +1,54 @@
+//stand alone scripts testing mongodb connections manually
+const mongoose = require('mongoose');
+
+
+if (process.argv.length <3) {
+    console.log('give password as argument');
+    process.exit(1);
+}
+
+//stores the password from command line argument
+const password = process.argv[2];
+const url = `mongodb+srv://Fabian:${password}@cluster0.esbajfd.mongodb.net/noteApp?retryWrites=true&w=majority`
+
+mongoose.set('strictQuery', false);
+mongoose.connect(url)
+  
+    
+    //blueprint that defines the structure of the documents in a collection
+    const noteSchema = new mongoose.Schema({
+        content: String,
+        important: Boolean,
+    })
+
+    //transform the returned object id property(mongoose object) to id(string) and remove _id and __v
+    noteSchema.set('toJSON', {
+        transform: (document, returnedObject) => {
+            returnedObject.id = returnedObject._id.toString()
+            delete returnedObject._id
+            delete returnedObject.__v
+        } 
+      })
+
+
+    //constructor function for creating and working with new documents in mongodb
+    // Note is the collection name in the database
+    module.exports =  mongoose.model('Note', noteSchema);
+
+    const note = new Note({
+        content: 'HTML is easy',
+        important: true,
+    })
+
+    //saves the note document to the database
+  //   note.save().then(result => {
+  //   console.log('note saved!')
+  // })
+ 
+  // retrieves all notes from the database and prints them to the console
+  Note.find({}).then(result => {
+    result.forEach(note => {
+      console.log(note)
+    })
+    mongoose.connection.close()
+  });
